dcfb5e386003f1426ddf0fde12ea4b596442222a
Sulstice <sharifsuliman1@gmail.com>
2024-07-17 11:18:27 -0500

    changed the downloads to wget

---
+++ stages/01_download_data.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+mkdir -p . /downloads
+
+wget -O downloads http://bioinf.xmu.edu.cn/ADReCS/download/v3.3/Drug_information_v3.3.xlsx
+wget -O downloads http://bioinf.xmu.edu.cn/ADReCS/download/v3.3/ADR_ontology_v3.3.xlsx
+wget -O downloads http://bioinf.xmu.edu.cn/ADReCS/download/v3.3/Drug_ADR_v3.3.txt.gz
+wget -O downloads http://bioinf.xmu.edu.cn/ADReCS/download/v3.3/Drug_ADR_Matrix_v3.3.txt.gz
+wget -O downloads http://bioinf.xmu.edu.cn/ADReCS/download/v3.3/ADReCS_ADR_Severity_Grade_v3.3.txt.gz
+wget -O downloads http://bioinf.xmu.edu.cn/ADReCS/download/v3.3/ADReCS_ADR_Frequency_v3.3.txt.gz
+wget -O downloads http://bioinf.xmu.edu.cn/ADReCS/download/v3.3/ADReCS_Drug_ADR_relations_quantification_v3.3.txt.gz
